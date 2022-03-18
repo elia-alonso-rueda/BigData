80f87c06cc27095142b942fcce901b9eb4c649f4
Itziar <ialonso@lst.tfo.upm.es>
2022-03-18 19:47:37 +0100

    concatenated

--- IREI/Assignment3/rank.py
+++ IREI/Assignment3/rank.py
@@ -25,21 +25,21 @@ import operator
 
 def segmentation_threshold(img):
     img_gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    img_eq = cv2.equalizeHist(img_gray)
+    # img_eq = cv2.equalizeHist(img_gray)
     def filter_image(image, mask):
         r = image[:,:,0] * mask
         g = image[:,:,1] * mask
         b = image[:,:,2] * mask
         return np.dstack([r,g,b])
     
-    thresh = threshold_otsu(img_eq)
-    img_otsu  = img_eq < thresh
+    thresh = threshold_otsu(img_gray)
+    img_otsu  = img_gray < thresh
     filtered = filter_image(img, img_otsu)
     return filtered
 #Read and show images
 
 # Define the variables
-list_images = [] # Create a list to store the images
+dict_images = {} # Create a dictionary to store the images
 array_target = [] # Create a list to store the names of the images
 pattern="db\.*.(\w+\d*).*" # Create a pattern to extract the names of the imahes
 
@@ -60,10 +60,9 @@ newImage = cv2.resize(cv2.imread(pathNewImage),(299, 299))
 for path in files:
     # Use the pattern to extract the name of the images
     match = re.search(pattern, path)
-    array_target.append(match.group(1))
     # Read the image and resize it in a common size
     img = cv2.resize(cv2.imread(path),(299, 299))
-    list_images.append(img)
+    dict_images[match.group(1)] = img
 
 
 #%%
@@ -76,39 +75,43 @@ print("1. Colors method \n")
 # Create the histogram for the input image
 hist_newImage = cv2.calcHist([newImage], [0], None, [20],[0, 256])
 
-list_hist = []
+dict_hist = {}
 # Create a loop to iterate the list of images
-for img in list_images:
+for img in dict_images:
     # Normalize the image
-    img_norm = cv2.normalize(img, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+    img_norm = cv2.normalize(dict_images[img], None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
     # Create a histogram for every image and store it in a list
     hist = cv2.calcHist([img_norm], [0], None, [20],[0, 256])
-    list_hist.append(hist)
+    dict_hist[img] = hist
 
 # Create a list to store the values of the comparations
 dict_corr = {}
-distances_table = [[0 for x in range(4)] for y in range(len(list_hist))]
+distances_table = [[0 for x in range(4)] for y in range(len(dict_hist))]
 # Define a list with the names of the comparation methods
 methods_names = ["Correlation", "Chi-square", "Intersection", "Bhattacharyya"]
 
 # Create a loop to iterate the list of histograms
-for hist in range(len(list_hist)):
+for hist in dict_hist:
     # Compare the histogram of the new image with the histograms of the images of the data base
-    corr = cv2.compareHist(hist_newImage, list_hist[hist], 0)
+    corr = cv2.compareHist(hist_newImage, dict_hist[hist], 0)
     # Store the result of the comparation
-    name = array_target[hist]
-    dict_corr[name] = round(corr, 4)
+    dict_corr[hist] = round(corr, 4)
 
 # Sort the images by correlation
 corr_sorted = sorted(dict_corr, key=dict_corr.get, reverse=True)
-print("corr_sorted", corr_sorted)
 # Printing the list of the three images with higher correlation using loop
 for w in corr_sorted:
     print(f"Image name: {w}, Correlaton: {dict_corr[w]}")
 
 thresh = round(len(corr_sorted)*0.15)
-print(f"Name of the {thresh} images with less correlation: {corr_sorted[0:thresh]}")
+names_thres = corr_sorted[0:thresh]
+dict_thres = {}
+print(f"Name of the {thresh} images with less correlation: {names_thres}")
 
+# Filter the dictionary of the images using the threshold
+for img in dict_images:
+    if (img in corr_sorted[0:thresh]):
+        dict_thres[img] = dict_images[img]
 
 # for hist in range(len(list_hist)):
 #     # Create a loop to iterate the list com methods for every histogram
@@ -142,15 +145,14 @@ hog_new_res = exposure.rescale_intensity(hog_new_img, in_range=(0, 10))
 dict_hog = {}
 
 # Create a loop to iterate the list of images
-for i in range(len(list_images)):
+for img in dict_thres:
     # Apply the method hog for each image
-    fd, hog_img = hog(list_images[i], orientations=9, pixels_per_cell=(6, 6), 
+    fd, hog_img = hog(dict_thres[img], orientations=9, pixels_per_cell=(6, 6), 
                     cells_per_block=(2, 2), visualize=True, multichannel=True)
     # Rescale histogram for better display 
     hog_res = exposure.rescale_intensity(hog_img, in_range=(0, 10))
     # Store the result in the dictionary
-    name= array_target[i]
-    dict_hog[name]=hog_res
+    dict_hog[img]=hog_res
 
 # Define a dictionary to store the euclidean distances
 distance_array={}
@@ -203,12 +205,11 @@ s_newImage = segmentation_threshold(newImage)
 dict_s = {}
 
 # Create a loop to iterate the list of images
-for i in range(len(list_images)):
+for img in dict_thres:
     # Threshold segmentation of all images
-    s = segmentation_threshold(list_images[i])
+    s = segmentation_threshold(dict_thres[img])
     # Store the result in the dictionary
-    name = array_target[i]
-    dict_s[name] = s
+    dict_s[img] = s
 
 # A) Compare with euclidean distance for every combination
 # Define a dictionary to store the euclidean distances
@@ -253,27 +254,44 @@ plt.show()
 # Calculate the histogram of the segmentated input image
 hist_seg_newImage = cv2.calcHist([s_newImage], [0], None, [5],[0, 256])
 
-# Define a list to store the histogram of the sementated images
-list_hist_seg = []
+# Define a dictionary to store the histogram of the sementated images
+list_hist_seg = {}
 # Create a loop to iterate the list of images segmentated
-for img in dict_s:
+for seg in dict_s:
     # Create a histogram for every image and store it in a list
-    hist = cv2.calcHist([dict_s[img]], [0], None, [5],[0, 256])
-    list_hist_seg.append(hist)
+    hist = cv2.calcHist([dict_s[seg]], [0], None, [5],[0, 256])
+    list_hist_seg[seg] = hist
 
-# Create a list to store the values of the comparations of the segmentated images
-distances_table_seg = [[0 for x in range(4)] for y in range(len(list_hist_seg))]
-
+dict_corr_seg = {}
 # Create a loop to iterate the list of segmentated histograms
-for hist in range(len(list_hist_seg)):
-    # Create a loop to iterate the list com methods for every histogram
-    for compare_method in range(4):
-        # Compare the histogram of the new image with the histograms of the images of the data base
-        base_base = cv2.compareHist(hist_seg_newImage, list_hist_seg[hist], compare_method)
-        # Store the result of the comparation
-        distances_table_seg[hist][compare_method] = round(base_base, 4) 
+for hist in list_hist_seg:
+    # Compare the histogram of the new image with the histograms of the images of the data base
+    corr_seg = cv2.compareHist(hist_seg_newImage, list_hist_seg[hist], 0)
+    # Store the result of the comparation
+    dict_corr_seg[hist] = round(corr_seg, 4)
 
-# Create a loop to print the results
-print("Methods:     ", methods_names)
-for i in range(len(array_target)):
-    print( f"    {array_target[i]}      {distances_table_seg[i]}")
+# Sort the images by correlation
+corr_seg_sorted = sorted(dict_corr_seg, key=dict_corr_seg.get, reverse=True)
+# Printing the list of the three images with higher correlation using loop
+for w in corr_seg_sorted:
+    print(f"Image name: {w}, Correlaton: {dict_corr_seg[w]}")
+
+print("Name of the three images with higher correlation: ", corr_seg_sorted[0:3])
+
+
+# # Create a list to store the values of the comparations of the segmentated images
+# distances_table_seg = [[0 for x in range(4)] for y in range(len(list_hist_seg))]
+
+# # Create a loop to iterate the list of segmentated histograms
+# for hist in range(len(list_hist_seg)):
+#     # Create a loop to iterate the list com methods for every histogram
+#     for compare_method in range(4):
+#         # Compare the histogram of the new image with the histograms of the images of the data base
+#         base_base = cv2.compareHist(hist_seg_newImage, list_hist_seg[hist], compare_method)
+#         # Store the result of the comparation
+#         distances_table_seg[hist][compare_method] = round(base_base, 4) 
+
+# # Create a loop to print the results
+# print("Methods:     ", methods_names)
+# for i in range(len(array_target)):
+#     print( f"    {array_target[i]}      {distances_table_seg[i]}")
